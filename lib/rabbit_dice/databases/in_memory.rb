94f111a2563fad9bb070e7ce0f3c3fdb51cd0e0f
Gilbert <gilbertbgarza@gmail.com>
2014-04-22 23:09:00 -0500

    Implement db#get_game

--- lib/rabbit_dice/databases/in_memory.rb
+++ lib/rabbit_dice/databases/in_memory.rb
@@ -2,13 +2,26 @@ module RabbitDice
   module Databases
     class InMemory
 
+      def initialize
+        @games = {}
+      end
+
       def create_game(attrs)
         game = Game.new(:players => attrs[:players])
+        @games[game.id] = game
 
         starting_player = game.players.sample
         starting_turn = Turn.new(:game_id => game.id, :player => starting_player)
         game.turns.push(starting_turn)
         game
+      end
+
+      def get_game(id)
+        # Since everything is in one object, this might be really messy later when we add persistence
+        # Or, if we choose the right database (*cough* document-oriented database *cough*) it might not!
+        # (A document-oriented database might be the right choice *for this project* because there is no
+        # more than one user playing the game at one time)
+        @games[id]
       end
 
     end
--- spec/database_spec.rb
+++ spec/database_spec.rb
@@ -14,5 +14,16 @@ describe RabbitDice::Databases::InMemory do
     expect(game.turns.first.rolls.count).to eq 0
   end
 
-  it "gets a game"
+  it "gets a game" do
+    created_game = db.create_game :players => ['Dan', 'Earl', 'Fred']
+    game = db.get_game(created_game.id)
+
+    # I'm repeating myself, but that's ok if it's one test. maybe
+    expect(game.players).to include('Dan', 'Earl', 'Fred')
+    expect(game.winner).to be_nil
+
+    expect(game.turns.count).to eq 1
+    expect(game.turns.first.player).to match /^Dan|Earl|Fred$/
+    expect(game.turns.first.rolls.count).to eq 0
+  end
 end
