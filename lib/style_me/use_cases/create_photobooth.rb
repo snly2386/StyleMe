d233e1b64ec8572c1a953716514284cbe1c07976
Christopher Palmer <snly2386@gmail.com>
2014-05-07 22:35:48 -0500

    testing

--- lib/style_me/use_cases/create_photobooth.rb
+++ lib/style_me/use_cases/create_photobooth.rb
@@ -7,11 +7,11 @@ module StyleMe
       @db = StyleMe.db
       # url = params[:url]
       @image_path = params[:image_file]
-      url = upload_to_s3
+      # url = upload_to_s3
       #-UPLOADED TO AMAZON S3 ------------------------------------
 
       #create photo with Amazon url and file path
-      photo = @db.create_photo(:url => url, :image_file => params[:image_file])
+      photo = @db.create_photo(:image_file => params[:image_file])
 
       #Retrieve photo with CAMFIND
       @token_response = Unirest::post "https://camfind.p.mashape.com/image_requests", 
@@ -28,7 +28,7 @@ module StyleMe
         "focus[x]" => "480",
         "focus[y]" => "640",
         # "image_request[image]" => File.new(url)
-        "image_request[image]" => File.new(params[:image_file])
+        "image_request[image]" => File.new('/Users/chrispalmer/Desktop/#{params[:image_file]}')
       }
 
       # def make_request
@@ -39,33 +39,33 @@ module StyleMe
       )
       # end
       
-      params[:url] = url
-      success :photo => photo, :results => @response, :photo_url => url, :user => user, :image_file => params[:image_file]
+      # params[:url] = url
+      success :photo => photo, :results => @response, :image_file => params[:image_file]
     end
 
-    def upload_to_s3
-      return failure(:invalid_url) if url.empty?
-      get_photo = @db.get_photo(@image_path)
-      user = @db.get_user(params[:user_id])
-      #UPLOAD TO AMAZON S3--------------------------------------
-      AWS.config(
-        :access_key_id => ENV['AWS_ACCESS_KEY_ID'], 
-        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-      )
+    # def upload_to_s3
+    #   return failure(:invalid_url) if url.empty?
+    #   get_photo = @db.get_photo(@image_path)
+    #   user = @db.get_user(params[:user_id])
+    #   #UPLOAD TO AMAZON S3--------------------------------------
+    #   AWS.config(
+    #     :access_key_id => ENV['AWS_ACCESS_KEY_ID'], 
+    #     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    #   )
 
-      bucket_name = 'chriswendystyle'
-      # file_name = 'images.jpeg'
-      file_name = get_photo.file_name
+    #   bucket_name = 'chriswendystyle'
+    #   # file_name = 'images.jpeg'
+    #   file_name = get_photo.file_name
 
-      # Get an instance of the S3 interface.
-      s3 = AWS::S3.new
+    #   # Get an instance of the S3 interface.
+    #   s3 = AWS::S3.new
 
-      # Upload a file.
-      key = File.basename(file_name)
-      s3.buckets[bucket_name].objects[key].write(:file => file_name)
-      url = File.join("https://s3.amazonaws.com/chriswendystyle", key)
-      puts "Uploading file #{file_name} to bucket #{bucket_name}."
-      return url
-    end
+    #   # Upload a file.
+    #   key = File.basename(file_name)
+    #   s3.buckets[bucket_name].objects[key].write(:file => file_name)
+    #   url = File.join("https://s3.amazonaws.com/chriswendystyle", key)
+    #   puts "Uploading file #{file_name} to bucket #{bucket_name}."
+    #   return url
+    # end
   end
 end
